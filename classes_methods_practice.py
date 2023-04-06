611a299c9cad25c61d7c0c724b22ad4236d3dac1
Ben Clark <114269513+Vytalic@users.noreply.github.com>
2023-04-06 18:44:13 -0500

    Create classes_methods_practice.py

---
+++ classes_methods_practice.py
@@ -0,0 +1,21 @@
+# More practice with Classes and Class methods
+# Used chatGPT to facilitate ideas regarding what kind of classes to make
+
+# Bank Account
+# deposit - withdraw - check balance
+
+
+# Student - name, age, and grade level
+# add/remove courses - calculating gpa
+
+
+# Employee - name, position, salary
+# giving raises - calculating taxes
+
+
+# Car - make, model, and year
+# starting - stopping - accelerating - check fuel level
+
+
+# Contact - names, phone number, and email address
+# add/remove contacts - search for contacts by name/phone number
