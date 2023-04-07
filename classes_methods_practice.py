2fb96b016757ee5b33bdefac8199f9c4f523199e
Ben Clark <114269513+Vytalic@users.noreply.github.com>
2023-04-06 20:02:27 -0500

    Update classes_methods_practice.py

--- classes_methods_practice.py
+++ classes_methods_practice.py
@@ -16,26 +16,111 @@ class Bank():
         self.balance = balance
         self.interest_rate = interest_rate
         
+                    
+    def wrong_pin(self):
+        print("\nError: You have entered an incorrect pin too many times. Exiting...\n")
+        return False
+    
+    
+    def check_pin(self):
+        attempts = int(0)
+        for attempt in range(5):            
+            if attempts > 0 and attempts != 5:
+                print(f"\nYou will be locked out after {5 - attempts} more tries.", end='')
+            code = int(input("\nEnter your pin: "))
+            if code == self.pin:
+                return True
+            elif code != self.pin:
+                attempts += 1
+                if attempts == 5:
+                    return False
+                 
+            
     def check_balance(self):
-        return(self.balance)
+        check_pin = self.check_pin()
+        if check_pin == True:
+            print("\n------------------------")
+            print(f"Welcome {self.owner}!")
+            print(f"Your balance is {self.balance}")
+            print("------------------------")
+        else:
+            self.wrong_pin()
+    
     
     def withdraw(self):
-        withdraw_x = int(input("\nWithdraw amount: "))
-        previous_bal = self.balance
-        self.balance += withdraw_x
-        print("\n\n----------------------------------------")
-        print("---------------Receipt------------------")
-        print("----------------------------------------")
-        print(f"-Owner: {self.owner}                      ")
-        print(f"-Account Number: {self.acct_num}                ")
-        print("-                                      ")
-        print(f"-Previous Balance: {previous_bal}                ")
-        print(f"-Withdrawal: {withdraw_x}                        ")
-        print(f"-New Balance: {self.balance}                     ")
-        print("----------------------------------------")
+        check_pin = self.check_pin()
+        if check_pin == True:
+            print(f"\nWelcome {self.owner}!")
+            withdraw_x = int(input("Withdraw amount: "))
+            previous_bal = self.balance
+            self.balance -= withdraw_x
+            print("\n\n----------------------------------------")
+            print("---------------Receipt------------------")
+            print("----------------------------------------")
+            print(f"-Owner: {self.owner}                      ")
+            print(f"-Account Number: {self.acct_num}                ")
+            print("-                                      ")
+            print(f"-Previous Balance: {previous_bal}                ")
+            print(f"-Withdrawal: {withdraw_x}                        ")
+            print(f"-New Balance: {self.balance}                     ")
+            print("----------------------------------------\n\n")
+        else:
+            self.wrong_pin()
 
-acct1 = Bank("Ben Clark", "222333", 7438, 1000, 0.025)
-acct1.withdraw()
+
+    def deposit(self):
+        check_pin = self.check_pin()
+        if check_pin == True:
+            print(f"\nWelcome {self.owner}!")
+            deposit_x = int(input("Deposit amount: "))
+            previous_bal = self.balance
+            self.balance += deposit_x
+            print("\n\n----------------------------------------")
+            print("---------------Receipt------------------")
+            print("----------------------------------------")
+            print(f"-Owner: {self.owner}                      ")
+            print(f"-Account Number: {self.acct_num}                ")
+            print("-                                      ")
+            print(f"-Previous Balance: {previous_bal}                ")
+            print(f"-Deposit: {deposit_x}                        ")
+            print(f"-New Balance: {self.balance}                     ")
+            print("----------------------------------------\n\n")
+        else:
+            self.wrong_pin()
+
+acct1 = Bank("Ben Clark", 222333, 7777, 1000, 0.025)
+done = False
+user_acct_num = int(input("\nPlease enter your account number: "))
+while done == False:
+    print("\n----------Menu----------")
+    print("------------------------")
+    print("1) Check Balance")
+    print("2) Withdraw")
+    print("3) Deposit")
+    print("4) Exit")
+    print("------------------------")
+    user_choice = int(input("\nChoose an option from above: "))
+    
+    if user_choice == 1:
+        acct1.check_balance()
+    elif user_choice == 2:
+       acct1.withdraw()
+    elif user_choice == 3:
+        acct1.deposit() 
+    elif user_choice == 4:
+        done = True
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Student - name, age, and grade level
 # add/remove courses - calculating gpa
